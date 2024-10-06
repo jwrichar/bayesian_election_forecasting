8bfa911d1cf9b48cd969d5996e9154fcb051b7d1
jwrichar <joeyrichar+github@gmail.com>
2024-10-05 20:24:17 -0700

    Change plots and poll updates.

--- source/plot_trends.R
+++ source/plot_trends.R
@@ -1,7 +1,7 @@
 plots_path = normalizePath("../plots")
 data_path = normalizePath("../data")
 
-dates = c('Sept 30', 'Oct 3')
+dates = c('Sept 30', 'Oct 4')
 
 date_old = gsub(' ', '',tolower(dates[1]))
 date_new = gsub(' ', '',tolower(dates[2]))
@@ -32,18 +32,31 @@ colvec[probs_new[is_swing_state] > probs_old[is_swing_state]] = 'blue'
 
 pdf(paste(
     plots_path, 
-    "/change_in_state_prob_", date_old, "_", date_new, "_", year, ".pdf",sep=""),
+    "/", year, "/change_in_state_prob_", date_old, "_", date_new, "_", year, ".pdf",sep=""),
 height=6,width=8)
 par(mar=c(5,3,5,1))
 plot(probs_old[is_swing_state],N_swing_state:1, pch=20, axes=FALSE, ylab="",
-     xlab="Probability of Democrat winning state",xlim=c(0,1), col=colvec,
+     xlab="Probability of Democrat winning state",
+     xlim=c(0,1), ylim=c(0.5, N_swing_state+1), col=colvec,
      main=paste("Change in Swing State Probabilities from", dates[1], "to", dates[2]))
+rect(0, 0, 0.2, N_swing_state+2, border=NA, col = rgb(1,0,0,0.2))
+rect(0.2, 0, 0.4, N_swing_state+2, border=NA, col = rgb(1,0,0,0.1))
+rect(0.6, 0, 0.8, N_swing_state+2, border=NA, col = rgb(0,0,1,0.1))
+rect(0.8, 0, 1, N_swing_state+2, border=NA, col = rgb(0,0,1,0.2))
+text(0.1, N_swing_state+1, 'Likely R', cex=0.7)
+text(0.3, N_swing_state+1, 'Leans R', cex=0.7)
+text(0.5, N_swing_state+1, 'Toss Up', cex=0.7)
+text(0.7, N_swing_state+1, 'Leans D', cex=0.7)
+text(0.9, N_swing_state+1, 'Likely D', cex=0.7)
 arrows(probs_old[is_swing_state], N_swing_state:1,
     probs_new[is_swing_state], N_swing_state:1, length=0.05, col=colvec ,lwd=4)
 box()
 axis(1)
 axis(2, at=N_swing_state:1,labels=state.abbrs[is_swing_state],las=1,cex.axis=0.9)
 abline(h=N_swing_state:1, col='#80808040')
-abline(v=0.5,col=4,lty=2,lwd=2)
+abline(v=0.5,col=4,lty=2,lwd=1)
 dev.off()
+
+
+
 
