145054d386177d06d15a8e4d660672900f017fa0
jwrichar <joeyrichar+github@gmail.com>
2024-10-08 21:04:58 -0700

    October 8 updates.

--- source/plot_trends.R
+++ source/plot_trends.R
@@ -1,7 +1,7 @@
 plots_path = normalizePath("../plots")
 data_path = normalizePath("../data")
 
-dates = c('Sept 30', 'Oct 3')
+dates = c('Sept 30', 'Oct 8')
 
 date_old = gsub(' ', '',tolower(dates[1]))
 date_new = gsub(' ', '',tolower(dates[2]))
